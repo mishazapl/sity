b554b49b4b9cbf7ecab700eb12697688bbaab68c
Misha Zapalenov <mzapalenov@mail.ru>
2017-08-29 11:26:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Hello my friend!</h1>
+	<p>You to learn the git?</p>
+	<p>Yes i to learn a git!</p>
+</body>
+</html>
